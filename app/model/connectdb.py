c0dd74821956b16d030e653a26ba96b3d0745de0
kave06 <kave006@gmail.com>
2017-11-16 20:23:30 +0100

    db conection and query test

--- app/model/connectdb.py
+++ app/model/connectdb.py
@@ -10,6 +10,11 @@ from time import sleep
 from re import match
 from app.model.logger import create_log
 
+DB_HOST = '157.88.58.134'
+DB_PORT = 5584
+DB_USER = 'kave'
+DB_PASS = 'hola'
+DB_NAME = 'pgpi'
 
 logger = create_log('controller.log')
 
@@ -18,16 +23,50 @@ logger = create_log('controller.log')
 #FLAG_RECONNECT = True
 
 
-def connect_db(host, user, db, password) -> pymysql.connect:
+def connect_db() -> pymysql.connect:
     global FLAG_RECONNECT
     cnx=''
     try:
-        cnx = pymysql.connect(host=host, port=3306, user=user,
-                              password=password, db=db)
-        logger.info('Connect to DB: ' + db)
+        cnx = pymysql.connect(host=DB_HOST, port=DB_PORT, user=DB_USER,
+                              password=DB_PASS, db=DB_NAME)
+        logger.info('Connect to DB: ' + DB_NAME)
     except MySQLError as err:
         logger.error(err)
        # FLAG_RECONNECT = True
     return cnx
 
 
+def select_user(user_name):
+
+    result_set = ''
+
+    # query = "SELECT user_username, user_password" \
+    #         "FROM users" \
+    #         "WHERE user_username = 'kave00'"
+
+    query3 = "SELECT * FROM users"
+
+
+    query = "SELECT * FROM users WHERE user_username = 'kave00'"
+
+    query2 = "SELECT user_username, user_password" \
+             "FROM users" \
+             "WHERE user_username = 'kave00'"
+    query4 = "SELECT * FROM users"
+
+    query5 = "SELECT user_username, user_password " \
+             "FROM users " \
+             "WHERE user_username = '{}'".format(user_name)
+    # Connect to db
+    cnx = connect_db()
+
+    try:
+        cursor = cnx.cursor()
+        cursor.execute(query5)
+        result_set = cursor.fetchmany(size=1)
+        cursor.close()
+    except Exception as err:
+        logger.error(err)
+
+    print('salida= {}'.format(result_set))
+    return result_set
--- manage.py
+++ manage.py
@@ -10,31 +10,34 @@ from flask_nav import Nav
 from flask_nav.elements import Navbar, View
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects import mysql
-from app.model.connectdb import connect_db
+from app.model.connectdb import connect_db, select_user
 
 # Global variables
 #todo esto tiene que ir a un archivo de configuración
-DB_HOST = 'localhost'
-#DB_PORT =
-DB_USER = 'root'
-DB_PASS = 'root'
-DB_NAME = 'incidencias'
-FLAG_RECONNECT = True
+# DB_HOST = 'localhost'
+# #DB_PORT =
+# DB_USER = 'root'
+# DB_PASS = 'root'
+# DB_NAME = 'incidencias'
+# FLAG_RECONNECT = True
 
 
-mysql = mysql()
 app = Flask(__name__)
 
-app.secret_key = '1234'
+# app.secret_key = '1234'
 
 # MySQL config
-app.config['MYSQL_DATABASE_USER'] = DB_USER
-app.config['MYSQL_DATABASE_PASSWORD'] = DB_PASS
-app.config['MYSQL_DATABASE_DB'] = DB_NAME
-app.config['MYSQL_DATABASE_HOST'] = DB_HOST
+# app.config['MYSQL_DATABASE_USER'] = DB_USER
+# app.config['MYSQL_DATABASE_PASSWORD'] = DB_PASS
+# app.config['MYSQL_DATABASE_DB'] = DB_NAME
+# app.config['MYSQL_DATABASE_HOST'] = DB_HOST
 
 
-
+DB_HOST = '157.88.58.134'
+DB_PORT = 5584
+DB_USER = 'kave'
+DB_PASS = 'hola'
+DB_NAME = 'cliente1'
 
 
 
@@ -45,20 +48,15 @@ nav = Nav()
 
 
 # validación de login
-@app.route('/')
-def validatelogin():
-    try:
-        _username = request.form['exampleInputEmail1']
-        _password = request.form['exampleInputPassword1']
-    except Exception as e:
-        return render_template('error.html')
-
-    con = connect_db(DB_HOST,DB_USER,DB_NAME,DB_PASS)
-    cursor = con.cursor()
-    cursor.callproc()
-
-
-
+# @app.route('/')
+# def validatelogin():
+#     try:
+#         _username = request.form['exampleInputEmail1']
+#         _password = request.form['exampleInputPassword1']
+#     except Exception as e:
+#         return render_template('error.html')
+#
+#     query = select_user()
 
 
 
@@ -74,6 +72,20 @@ def tarjeta():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/test')
+def test():
+    # _username = request.form['exampleInputEmail1']
+    # _password = request.form['exampleInputPassword1']
+    select_user('kave00')
+
+
+
+
+    return render_template('login.html')
+
+
 
 
 if __name__ == '__main__':
