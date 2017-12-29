ff707f28efd15013e52ec29d15e0f18fa8cbaf2d
kave06 <kave006@gmail.com>
2017-12-29 16:27:45 +0100

    cambios en db_notify

--- app/model/db_notify.py
+++ app/model/db_notify.py
@@ -2,14 +2,15 @@ from peewee import SqliteDatabase, Model, CharField
 
 db = SqliteDatabase('notification.db')
 
+
 class Notification(Model):
     incidence_id = CharField()
     sender = CharField()
     receiver = CharField()
 
-
     class Meta:
         database = db
+
 
 db.connect()
 
@@ -21,9 +22,11 @@ def create_notification(incidence_id, sender, receiver):
     with db.atomic():
         Notification.create(incidence_id=incidence_id, sender=sender, receiver=receiver)
 
-def get_notification(receiver:str):
-    return Notification.select().where(Notification.receiver==receiver)
-     #     return list(Notification.select().where(Notification.receiver==receiver))
+
+def get_notification(receiver: str):
+    return list(Notification.select().where(Notification.receiver == receiver))
+
+
 def delete_notification(incidence_id, sender):
-    Notification.delete().where(Notification.incidence_id==incidence_id and
-                                         Notification.sender==sender)
+    Notification.delete().where(Notification.incidence_id == incidence_id and
+                                Notification.sender == sender)
